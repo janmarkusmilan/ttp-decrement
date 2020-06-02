5435865d5abcb7372d3175755f8b443dc8aa4ab7
Jan Markus Milan <jmarkusm@me.com>
2020-06-02 16:08:30 -0400

    Added the code for App.js and Decrement.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Decrement from "./Decrement";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<>
+				<Decrement start={10} />
+				<Decrement start={20} />
+				<Decrement start={30} />
+				<Decrement start={40} />
+				<Decrement start={50} />
+			</>
+		);
+	}
 }
 
 export default App;
